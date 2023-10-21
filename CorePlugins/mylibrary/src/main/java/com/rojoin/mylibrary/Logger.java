b97a960bec1d5f4efaec81256bc7dc83a6d38e05
Rojoin <nachoarrastua@gmail.com>
2023-10-20 22:14:42 -0300

    Add Save to log method

--- CorePlugins/mylibrary/src/main/java/com/rojoin/mylibrary/Logger.java
+++ CorePlugins/mylibrary/src/main/java/com/rojoin/mylibrary/Logger.java
@@ -1,10 +1,14 @@
 package com.rojoin.mylibrary;
 
+import android.os.Environment;
 import android.util.Log;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.io.File;
+import java.io.FileWriter;
 public class Logger {
 
     private static final String LOGTAG = "RojoinLOG";
@@ -30,5 +34,18 @@ public class Logger {
     public void ClearLogs()
     {
         logList.clear();
+    }
+    private void SaveLogsToFile() {
+        File logFile = new File(Environment.getExternalStorageDirectory(), "unity_logs.txt");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true));
+            for (String log : logList) {
+                writer.write(log);
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
