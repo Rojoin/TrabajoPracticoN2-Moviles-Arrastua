4470087d41d274507b14da925f8babbed2657fa7
Rojoin <nachoarrastua@gmail.com>
2023-10-22 18:14:34 -0300

    Add test for differentErrors

--- Assets/TestPluginLog.cs
+++ Assets/TestPluginLog.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using TMPro;
 using UnityEngine;
@@ -10,7 +11,9 @@ public class TestPluginLog : MonoBehaviour
     private const string packageName = "com.rojoin.UnityLogger";
     private const string className = packageName + ".Logger";
     [SerializeField] private TextMeshProUGUI textBox;
-
+    public GameObject textMeshProPrefab;
+    public GameObject contentParent;
+    private List<GameObject> logsList = new List<GameObject>();
 #if UNITY_ANDROID
     private AndroidJavaClass PluginClass;
     private AndroidJavaClass unityPlayer;
@@ -50,23 +53,26 @@ public class TestPluginLog : MonoBehaviour
 
     private void SendLogToAndroid(string logString, string stackTrace, LogType type)
     {
-        switch (type)
+        if (Application.platform == RuntimePlatform.Android)
         {
-            case LogType.Error:
-                pluginInstance.Call("SendLog", logString, 2);
-                break;
-            case LogType.Assert:
-                pluginInstance.Call("SendLog", logString);
-                break;
-            case LogType.Warning:
-                pluginInstance.Call("SendLog", logString, 1);
-                break;
-            case LogType.Log:
-                pluginInstance.Call("SendLog", logString, 0);
-                break;
-            case LogType.Exception:
-                pluginInstance.Call("SendLog", logString, 3);
-                break;
+            switch (type)
+            {
+                case LogType.Error:
+                    pluginInstance.Call("SendLog", logString, 2);
+                    break;
+                case LogType.Assert:
+                    pluginInstance.Call("SendLog", logString);
+                    break;
+                case LogType.Warning:
+                    pluginInstance.Call("SendLog", logString, 1);
+                    break;
+                case LogType.Log:
+                    pluginInstance.Call("SendLog", logString, 0);
+                    break;
+                case LogType.Exception:
+                    pluginInstance.Call("SendLog", logString, 3);
+                    break;
+            }
         }
     }
 
@@ -75,6 +81,23 @@ public class TestPluginLog : MonoBehaviour
         if (Application.platform == RuntimePlatform.Android)
         {
             pluginInstance.Call("ShowAlert");
+        }
+    }
+
+    public void ReadLogs()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            string logs = pluginInstance.Call<string>("readFile");
+            string[] lines = logs.Split(new[] { "\n", "\r\n" }, System.StringSplitOptions.None);
+            logsList.Clear();
+            foreach (string line in lines)
+            {
+                GameObject textObject = Instantiate(textMeshProPrefab, transform);
+                TextMeshProUGUI textComponent = textObject.GetComponent<TextMeshProUGUI>();
+                textComponent.text = line;
+                logsList.Add(textObject);
+            }
         }
     }
 
@@ -93,6 +116,23 @@ public class TestPluginLog : MonoBehaviour
         }
     }
 
-
-
+    public void TestWarning()
+    {
+        string text = "This is a Warning";
+        textBox.text = text;
+        Debug.LogWarning(text);
+    } 
+    public void TestLog()
+    {
+        string text = "This is a Log";
+        textBox.text = text;
+        Debug.LogWarning(text);
+    }
+    public void TestError()
+    {
+        string text = "This is a Error";
+        textBox.text = text;
+        Debug.LogError(text);
+    }
+  
 }
